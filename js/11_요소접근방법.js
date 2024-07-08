5f79c1da7f29f686265f289fe9419a2428440397
limsehun <tpgns8714852@gmail.com>
2024-07-08 15:50:33 +0900

    6교시 완

--- js/11_요소접근방법.js
+++ js/11_요소접근방법.js
@@ -22,3 +22,53 @@ function classTest(){
         
     }
 }
+
+// input에 입력된 숫자 모두 얻어와 합계 구하기
+function classTest2() {
+    
+
+
+    /*  input 요소에 입력된 값 얻어오는 방법
+     -> input요소.value
+
+      HTML 요소에서 얻어온 값의 자료형은 
+     -> 모두 string(문자열)
+
+      합계 구하는 방법
+     -> let sum = 0; 선언 후 누적 
+
+    HTML 문서에서 class 속성값이 일치하는 요소 모두 얻어오기
+     -> document.getElementsByClassName("class속성값")
+       -> HTMLCollection (유사배열) 형태로 반환
+    */
+
+    const input = document.getElementsByClassName("cls-test2");
+
+    let sum = 0;
+
+    for(let i = 0; i < input.length ; i++){
+
+        sum += Number(input[i].value);   //합계 누적
+    }
+
+    alert(`합계 : ${sum}`);
+}
+
+
+/** 태그명으로 요소 접근하기 */
+function tagNameTest(){
+
+    /* document.getElmentsByTagName("태그명") */
+    const tagList = document.getElementsByTagName("li");
+    // -> 요소 4개짜리 배열 변환
+    // i == 0,1,2,3
+    for(let i=0 ; i<tagList.length ; i++){
+      // tagList[i].innerText : 작성된 내용(색상)
+
+      //글자색 변경
+      tagList[i].style.color = tagList[i].innerText;
+
+      //태두리 변경
+      tagList[i].style.border = `3px solid ${tagList[i].innerText}`;
+  }
+}
