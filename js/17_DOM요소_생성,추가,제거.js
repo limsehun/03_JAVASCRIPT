d8ab72fce726817d6f87ec4ed3c9a210ad3c590c
limsehun <tpgns8714852@gmail.com>
2024-07-11 16:53:17 +0900

    7교시 완

--- js/17_DOM요소_생성,추가,제거.js
+++ js/17_DOM요소_생성,추가,제거.js
@@ -27,6 +27,28 @@ for (let i = 0; i < btns.length; i++) {
                 span2.innerText=count;
                 standard.append(span2); //#standard의 마지막 자식으로 span2추가
             break;
+
+            case "before":
+                const div1 = document.createElement("div"); //div 요소 생성
+                div1.innerText=count; //내용으로 count추가
+                div1.style.backgroundColor="greenyellow"; //배경색 추가
+                div1.className="box"; // box 클래스 추가
+                standard.before(div1); //#standard 앞쪽에 추가
+            break;
+
+            case "after":
+                const div2 = document.createElement("div");
+                div2.innerText = count;
+                div2.style.backgroundColor = "orange";
+                div2.className = "box";
+                standard.after(div2); // #strandard 뒷쪽에 추가
+            break;
+
+            case "remove":
+                // .container 요소를 제거
+                document.querySelector(".container").remove();
+            break;
+
         }
 
         count++; // count 1 증가
